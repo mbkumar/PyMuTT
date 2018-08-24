78e940097585f4168f9cd4b79b351f4a8a5174ee
Bharat Medasani <mbkumar@gmail.com>
2018-08-23 22:48:32 -0400

    Doc strings and code in harmonicthermo cleaned

--- PyMuTT/models/statmech/harmonicthermo.py
+++ PyMuTT/models/statmech/harmonicthermo.py
@@ -1,129 +1,94 @@
 # -*- coding: utf-8 -*-
-"""
-PyMuTT.models.statmech.harmonicthermo
-
-Vlachos group code for Harmonic approximation.
-"""
+""" Code for Harmonic approximation. """
 
 import numpy as np
 from ase import thermochemistry
 from PyMuTT import constants as c
-from PyMuTT.models.statmech.heat_capacity import get_CvoR_trans, get_CvoR_vib, get_CvoR_rot
+from PyMuTT.models.statmech.heat_capacity import get_CvoR_trans, \
+    get_CvoR_vib, get_CvoR_rot
 
 class HarmonicThermo:
-	"""Treats all degrees of freedom harmonically. Uses ase.thermochemistry.HarmonicThermo to calculate	enthalpy and entropy
+    """Calculate enthalpy and entropy using harmonic approach.
 
-	Attributes
-	----------
-		model : `ase.thermochemistry.HarmonicThermo`_
+    Depends on ASE thermochemistry module.
+    """
+    def __init__(self, vib_energies, pot_energy=0.0):
+        """
 
-	Below are parameters accepted by `ase.thermochemistry.HarmonicThermo`_
+        Args:
+            vib_energies (numpy array): Vibrational energies in eV
+            pot_energy (float): Potential energy in eV
+        """
+        self.model = thermochemistry.HarmonicThermo(vib_energies=vib_energies,
+                                                    potentialenergy=pot_energy)
 
-	================= ==============================================================================
-	Parameter         Description
-	================= ==============================================================================
-	vib_energies      ((3N,) `numpy.ndarray`_ where N is number of atoms) Vibrational energies in eV
-	potentialenergy   (float) Potential energy in eV
-	================= ==============================================================================
-	
-	.. _`ase.thermochemistry.HarmonicThermo`: https://wiki.fysik.dtu.dk/ase/ase/thermochemistry/thermochemistry.html#ase.thermochemistry.HarmonicThermo
-	.. _`numpy.ndarray`: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.ndarray.html
-	"""
-	def __init__(self, vib_energies, potentialenergy=0.0):
-		self.model = thermochemistry.HarmonicThermo(
-			vib_energies = vib_energies,
-			potentialenergy = potentialenergy)
+    def get_CpoR(self, Ts):
+        """Calculate dimensionless heat capacity (Cp/R).
 
-	def get_CpoR(self, Ts):
-		"""Calculates the dimensionless heat capacity (Cp/R) at a given temperature.
+        Args:
+            Ts (float or numpy array): Temperature(s) in K
+        Returns:
+            CpoR (float or numpy array): Dimensionless heat capacity (Cp/R)
+        """
+        return get_CvoR_vib(vib_energies=self.model.vib_energies, Ts=Ts)
 
-		Parameters
-		----------
-			Ts : float or (N,) `numpy.ndarray`_
-				Temperature(s) in K
-		Returns
-		-------
-			CpoR : float or (N,) `numpy.ndarray`_
-				Dimensionless heat capacity (Cp/R)
+    def get_HoRT(self, Ts, verbose = False):
+        """Calculate dimensionless enthalpy at a given temperature.
 
-		.. _`numpy.ndarray`: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.ndarray.html
-		""" 
-		return get_CvoR_vib(vib_energies=self.model.vib_energies, Ts=Ts)
+        Args:
+            Ts (float or numpy array): Temperature(s) in K
+            verbose (bool): Flag to print each contribution to enthalpy
+        Returns:
+            HoRT (float or numpy array): Dimensionless heat capacity (H/RT)
+        """
+        try:
+            iter(Ts)
+        except TypeError:
+            HoRT = self.model.get_internal_energy(Ts, verbose) / \
+                   (c.kb('eV/K') * Ts)
+        else:
+            HoRT = np.zeros_like(Ts)
+            for i, T in enumerate(Ts):
+                HoRT[i] = self.model.get_internal_energy(T, verbose) / \
+                          (c.kb('eV/K') * T)
+        return HoRT
 
-	def get_HoRT(self, Ts, verbose = False):
-		"""Calculates the dimensionless enthalpy at a given temperature
+    def get_SoR(self, Ts, verbose=False):
+        """Calculate dimensionless entropy at a given temperature.
 
-		Parameters
-		----------
-			Ts : float or (N,) `numpy.ndarray`_
-				Temperature(s) in K
-			verbose : bool
-				Whether a table breaking down each contribution should be printed
-		Returns
-		-------
-			HoRT : float or (N,) `numpy.ndarray`_
-				Dimensionless heat capacity (H/RT) at the specified temperature
+        Args:
+            Ts (float or numpy array): Temperature(s) in K
+            verbose (bool): Flag to print each contribution to entropy
+        Returns:
+            SoR (float or numpy array): Dimensionless entropy (S/R)
+        """
+        try:
+            iter(Ts)
+        except TypeError:
+            SoR = self.model.get_entropy(Ts, verbose)/c.R('eV/K')
+        else:
+            SoR = np.zeros_like(Ts)
+            for i, T in enumerate(Ts):
+                SoR[i] = self.model.get_entropy(T, verbose)/c.R('eV/K')
+        return SoR
 
-		.. _`numpy.ndarray`: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.ndarray.html
-		"""
-		try:
-			iter(Ts)
-		except TypeError:
-				HoRT = self.model.get_internal_energy(temperature=Ts, verbose=verbose)/(c.kb('eV/K') * Ts)
-		else:
-			HoRT = np.zeros_like(Ts)
-			for i, T in enumerate(Ts):
-				HoRT[i] = self.model.get_internal_energy(temperature=T, verbose=verbose)/(c.kb('eV/K') * T)
-		return HoRT
+    def get_GoRT(self, Ts, verbose=False):
+        """Calculate dimensionless Gibbs energy at a given temperature.
 
-	def get_SoR(self, Ts, verbose=False):
-		"""Returns the dimensionless entropy at a given temperature and pressure
-
-		Parameters
-		----------
-			Ts : float or (N,) `numpy.ndarray`_ 
-				Temperature(s) in K
-			verbose : bool
-				Whether a table breaking down each contribution should be printed
-		Returns
-		-------
-			SoR : float or (N,) `numpy.ndarray`_
-				Dimensionless entropy (S/R) at the specified temperature and pressure
-
-		.. _`numpy.ndarray`: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.ndarray.html
-		"""
-		try:
-			iter(Ts)
-		except TypeError:
-			SoR = self.model.get_entropy(temperature=Ts, verbose=verbose)/c.R('eV/K')
-		else:
-			SoR = np.zeros_like(Ts)
-			for i, T in enumerate(Ts):
-				SoR[i] = self.model.get_entropy(temperature=T, verbose=verbose)/c.R('eV/K')
-		return SoR
-
-	def get_GoRT(self, Ts, verbose=False):
-		"""Returns the dimensionless Gibbs energy at a given temperature
-
-		Parameters
-		----------
-			Ts : float or (N,) `numpy.ndarray`_
-				Temperature(s) in K
-			verbose : bool
-				Whether a table breaking down each contribution should be printed
-		Returns
-		-------
-			GoRT : float or (N,) `numpy.ndarray`_
-				Dimensionless heat capacity (G/RT) at the specified temperature
-
-		.. _`numpy.ndarray`: https://docs.scipy.org/doc/numpy-1.14.0/reference/generated/numpy.ndarray.html
-		"""
-		try:
-			iter(Ts)
-		except TypeError:
-			GoRT = self.model.get_helmholtz_energy(temperature=Ts, verbose=verbose)/(c.kb('eV/K') * Ts)
-		else:
-			GoRT = np.zeros_like(Ts)
-			for i, T in enumerate(Ts):
-				GoRT[i] = self.model.get_helmholtz_energy(temperature=T, verbose=verbose)/(c.kb('eV/K') * T)
-		return GoRT
+        Args:
+            Ts (float or numpy array): Temperature(s) in K
+            verbose (bool): Flag to print each contribution to enthalpy
+        Returns:
+            GoRT (float or numpy array): Dimensionless heat capacity (H/RT)
+        """
+        try:
+            iter(Ts)
+        except TypeError:
+            GoRT = self.model.get_helmholtz_energy(Ts, verbose) / \
+                   (c.kb('eV/K') * Ts)
+        else:
+            GoRT = np.zeros_like(Ts)
+            for i, T in enumerate(Ts):
+                GoRT[i] = self.model.get_helmholtz_energy(T, verbose) / \
+                          (c.kb('eV/K') * T)
+        return GoRT
